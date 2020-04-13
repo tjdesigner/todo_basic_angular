a08afb417c35ada1cceaad5b8a883ceb2f8f68d1
Tiago de Jesus <tiagodejesus.design@gmail.com>
2020-04-13 09:00:43 -0300

    Primeiro commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, ReactiveFormsModule, FontAwesomeModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/models/todo.models.ts
@@ -0,0 +1,3 @@
+export class Todo {
+  constructor(public id: Number, public title: String, public done: Boolean) {}
+}
